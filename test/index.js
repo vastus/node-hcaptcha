ba2d650d689bcd3ef240cc42940713951a5dbbbc
Juho Hautala <juho.hautala@helsinki.fi>
2018-10-09 19:30:30 +0300

    Add test for the verify fn

--- test/index.js
+++ test/index.js
@@ -1,8 +1,24 @@
 const assert = require('assert');
+const nock = require('nock');
+const {verify} = require('../index');
+
+// mock the request
+nock('https://hcaptcha.com')
+  .post('/siteverify', 'secret=secret&response=token')
+  .reply(200, '{"success":true}')
 
 describe('hCaptcha', function () {
-  it('works', function () {
-    assert.ok(true);
+  describe('verify', function () {
+    const secret = 'secret';
+    const token = 'token';
+
+    it('resolves on success', function (done) {
+      verify(secret, token)
+        .then((data) => {
+          assert.deepStrictEqual(data, {success: true});
+          done();
+        })
+        .catch(done);
+    });
   });
 });
-
